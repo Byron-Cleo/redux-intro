5cb1ca2503ff7eb6d4e169484b8d6f817ffd1f1f
Byron Ochara <byron.ochara@gmail.com>
2025-10-11 23:28:37 +0300

    step one of actions mutating the state manually defining the values

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ const initialState = {
   loanPurpose: "",
 };
 
-//reducer is for update state with action types
+//reducer to update state based on actions to be dispatched
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
@@ -16,11 +16,16 @@ function reducer(state = initialState, action) {
     case "account/requestLoan":
       if (state.loan > 0) return state;
       //LATER
-      return { ...state };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
-        laon: 0,
+        loan: 0,
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
@@ -28,3 +33,26 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+//STEP 1
+//created the store for global state; that can be accessed from anywhere
+//This is the manual way of demnstrating how the action mutate the state hence will be replaced
+//with action creators in step 2
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+//advanced payload as an object to pass many pieces of data
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    purpose: "Buy a house",
+  },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan"});
+console.log(store.getState());
