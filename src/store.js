325e8cefb497ad7e37f7aa9ec6cd51160a1c9181
Byron Ochara <byron.ochara@gmail.com>
2025-10-12 17:12:28 +0300

    application of reduxToolkit automatically doing operations behind the scenes

--- src/store.js
+++ src/store.js
@@ -1,18 +1,9 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import {thunk} from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
 });
-//STEP 1
-//created the store for global state; that can be accessed from anywhere
-//the thunk in place is used by the application as a middleware which sits in between the reducers(STORE) and the actions
-// to do the computations and side effects as required by the application being built
-//HERE WE ARE SAYING WE WANT TO USE THUNK IN OUR APPLICATION
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
