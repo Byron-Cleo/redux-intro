0d8e8e4f81e2cd5297aff93879389aa01ee81676
Byron Ochara <byron.ochara@gmail.com>
2025-10-12 15:43:28 +0300

    implemented a thunk middleware

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,9 +1,10 @@
+//THE GLOBAL STATE
 const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
-
+//STEP 1: REDUCER TO UPDATE THE GLOBAL STATE
 //reducer to update state based on actions to be dispatched
 //NB: THE REDUCER SHOULD NOT HAVE A SIDE EFFECT i.e which is computing capabilites
 export default function accountReducer(state = initialStateAccount, action) {
@@ -33,11 +34,26 @@ export default function accountReducer(state = initialStateAccount, action) {
   }
 }
 
+//ACTIONS
 //STEP 2: ACTION CREATOR FUNCTIONS THAT ANUTOMATICALLY DISPATCH THE ACTION TYPES WITH
 //USER DEFINED VALUES
 //these functions return actions which are the objects being defined in step 1
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+//here we can do as much logic as possible and even side effects like fetching data from the API
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  //THIS IS THE MIDLLEWARE SITTING BETWEEN THE DISPATCH AND THE STORE
+  return async function (dispatch, getState) {
+    //API call
+    const url = `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    const convertedAmount = (amount * data.rates.USD).toFixed(4);
+
+    //dispatch with final value to update the state being determined and the reducer
+    dispatch({ type: "account/deposit", payload: convertedAmount });
+  };
 }
 
 export function withdraw(amount) {
